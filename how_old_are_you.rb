d4d201972c7b97ce6aadff5b7b5a612e3b09eb1d
Deja Monae Allison <dejaallison@gmail.com>
2020-06-09 01:21:54 +0000

    Automatically backed up by Learn

--- how_old_are_you.rb
+++ how_old_are_you.rb
@@ -1,5 +1,6 @@
 require_relative './current_age_for_birth_year.rb'
 def current_age_for_birth_year(birth_year)
+   Time.now.year - birth_year
 end
 puts "What year were you born?"
 birth_year = gets.to_i
